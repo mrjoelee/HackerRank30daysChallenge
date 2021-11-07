e4b5e3dddcef672545bdf01468f22b57873c29f8
mrjoelee <90262428+mrjoelee@users.noreply.github.com>
2021-11-07 17:05:43 -0500

    Day 03 and Day 04

---
+++ Day03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Day03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            //N is odd
+            if(N % 2 == 1)
+            {
+                Console.WriteLine("Weird");
+            }
+            //N is even and is between 2 and 5
+            else if(N % 2 == 0 && N > 2 && N < 5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            //N is even and is between 6 and 20
+            else if(N % 2 == 0 && N > 6 && N <= 20)
+            {
+                Console.WriteLine("Weird");
+            }
+            //N is even and is greater than 20
+            else if(N % 2 == 0 && N > 20)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Day04- Class vs Instance/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Day04__Class_vs_Instance
+{
+    class Person
+    {
+        int age;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an age:");
+            int T = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < T; i++)
+            {
+                Person p = new Person(T);
+                p.AmIOld();
+                for (int j = 0; j < 3; j++)
+                {
+                    p.yearPasses();
+                }
+                p.AmIOld();
+                Console.WriteLine();
+            }
+
+        }
+
+        public Person(int initialAge)
+        {
+           if (initialAge >= 0)
+            {
+                age = initialAge;
+            }
+            else
+            {
+                Console.WriteLine("Age is not valid, setting age to 0.");
+            }
+        }
+
+        public void AmIOld()
+        {
+            if (age < 13)
+            {
+                Console.WriteLine("You are young.");
+            }
+            else if (age >= 13 && age < 18)
+            {
+                Console.WriteLine("You are a teenager.");
+            }
+            else
+            {
+                Console.WriteLine("You are old.");
+            }
+        }
+
+        public void yearPasses()
+        {
+            age = age + 1;
+        }
+    }
+}
