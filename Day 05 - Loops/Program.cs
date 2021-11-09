7b70287988300bb61350b309ff249f0e61c9f1c4
mrjoelee <90262428+mrjoelee@users.noreply.github.com>
2021-11-09 08:52:41 -0500

    Day05 HackerRank

---
+++ Day 05 - Loops/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day_05___Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(n + " x " + i + " = " + (n * i));
+            }
+            Console.Read();
+            
+        }
+    }
+}
